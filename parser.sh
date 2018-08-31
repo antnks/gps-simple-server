9e74a2a7ab03e9ceda763192b3f1481c62b9da1e
antnks <antnks@users.noreply.github.com>
2018-08-31 16:24:11 +0300

    Moved file names into variables, added log rotation (gzip in the same dir)

--- parser.sh
+++ parser.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
+FILELOGOK=/path/to/server
+FILELOGER=/path/to/server_error.log
+LOGROTATE=30000000
+
 while read -d '#' line; do
 	sig="$(echo $line | head -c 1)"
+
 	if [ "$sig" == "*" ]
 	then
-		printf '%s\n' "$line" >> server.txt
+
+		sz=`stat --printf="%s" $FILELOGOK.csv`
+		if [ "$sz" -gt "$LOGROTATE" ]
+		then
+			backup=`date +"%Y%m%d%H%M%S"`
+			gzip -c $FILELOGOK.csv > ${FILELOGOK}_${backup}.csv.gz
+			rm $FILELOGOK.csv
+		fi
+
+		printf '%s\n' "$line" >> $FILELOGOK.csv
 	else
-		printf '%s\n' "$line" >> server_error.txt
+		printf '%s\n' "$line" >> $FILELOGER
 		exit 1
 	fi
 done
