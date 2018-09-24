cbf15c9fb9295612729488c50b84ce52535b1835
antnks <antnks@users.noreply.github.com>
2018-09-24 11:46:15 +0300

    Refactored server code to support more protocols

--- server.sh
+++ server.sh
@@ -1,27 +1,40 @@
 #!/bin/bash
 
-FILELOGOK=/path/to/server
-FILELOGER=/path/to/server_error.log
+DB_H0_OK=/path/to/server
+DB_H0_ER=/path/to/server_error.log
+
+DELIM_H0="#"
+
+SIG_H0="*"
+
 LOGROTATE=30000000
 
-while read -d '#' line; do
+# delimeters of supported protocols
+IFS="$DELIM_H0"
+
+touch $DB_H0_OK.csv
+touch $DB_H0_ER
+
+while read line
+do
 	sig="$(echo $line | head -c 1)"
 
-	if [ "$sig" == "*" ]
+	if [ "$sig" == "$SIG_H0" ]
 	then
-
-		sz=`stat --printf="%s" $FILELOGOK.csv`
-		if [ "$sz" -gt "$LOGROTATE" ]
-		then
-			backup=`date +"%Y%m%d%H%M%S"`
-			gzip -c $FILELOGOK.csv > ${FILELOGOK}_${backup}.csv.gz
-			rm $FILELOGOK.csv
-		fi
-
-		printf '%s\n' "$line" >> $FILELOGOK.csv
+		printf '%s\n' "$line" >> $DB_H0_OK.csv
 	else
-		printf '%s\n' "$line" >> $FILELOGER
+		printf '%s\n' "$line" >> $DB_H0_ER
 		exit 1
 	fi
+
+	# db rotate
+	sz=`stat --printf="%s" $DB_H0_OK.csv`
+	if [ "$sz" -gt "$LOGROTATE" ]
+	then
+		backup=`date +"%Y%m%d%H%M%S"`
+		gzip -c $DB_H0_OK.csv > ${DB_H0_OK}_${backup}.csv.gz
+		rm $DB_H0_OK.csv
+	fi
+
 done
 
